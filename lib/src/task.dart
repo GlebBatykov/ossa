b5ec70f27ca2b7712caf28f1848265acfb5ee735
Gleb Batykov <gleb.batykov@gmail.com>
2022-03-20 01:49:33 +0300

    Adds task library file

---
+++ lib/src/task.dart
@@ -0,0 +1,21 @@
+library theater_task.task;
+
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:theater_task/src/isolate.dart';
+
+import 'core.dart';
+
+part 'task/task.dart';
+part 'task/task_event.dart';
+part 'task/task_status.dart';
+part 'task/task_action.dart';
+part 'task/task_context.dart';
+part 'task/task_isolate_handler.dart';
+part 'task/task_error.dart';
+part 'task/task_type.dart';
+
+part 'task/exception/task_context_exception.dart';
+part 'task/exception/task_exception.dart';
+part 'task/exception/task_complete_exception.dart';
