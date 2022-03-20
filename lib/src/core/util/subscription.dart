496e05bcfcab5c710041b666a7726746b53c1720
Gleb Batykov <gleb.batykov@gmail.com>
2022-03-20 22:23:56 +0300

    Renames error subscription class

--- lib/src/core/util/subscription.dart
+++ lib/src/core/util/subscription.dart
@@ -1,14 +1,13 @@
 part of theater_task.core;
 
-class ErrorSubscription {
+class Subscription {
   bool _isCancel = false;
 
   bool get isCancel => _isCancel;
 
   final StreamSubscription _subscription;
 
-  ErrorSubscription(StreamSubscription subscription)
-      : _subscription = subscription;
+  Subscription(StreamSubscription subscription) : _subscription = subscription;
 
   void cancel() {
     _subscription.cancel();
