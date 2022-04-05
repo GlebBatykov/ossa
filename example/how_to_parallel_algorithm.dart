59d4076b5b94cf852a09db493db07f6f20c4bdfb
Batykov Gleb <gleb.batykov@gmail.com>
2022-04-05 10:07:54 +0300

    Updates example

--- example/how_to_parallel_algorithm.dart
+++ example/how_to_parallel_algorithm.dart
@@ -4,11 +4,10 @@ void main() async {
   // We have some initial data
   var data = List<double>.generate(5, (index) => index.toDouble());
 
-  //
   var tasks = <Task<double>>[];
 
   for (var i = 0; i < data.length; i++) {
-    //
+    // Create and run task with run method, passing data to task
     tasks.add(await Task.run<double>((context) {
       var number = context.get<double>('number');
 
@@ -16,11 +15,10 @@ void main() async {
     }, data: {'number': data[i]}));
   }
 
-  //
+  // Wait when all task is completed
   var results = await Future.wait<double>(List<Future<double>>.generate(
       tasks.length, (index) => tasks[index].result()));
 
-  //
   for (var result in results) {
     print(result);
   }
