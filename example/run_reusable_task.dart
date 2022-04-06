f7b221eaf5ef32b338306a0d60f5bb6a33f728a1
Gleb Batykov <gleb.batykov@gmail.com>
2022-04-06 18:16:48 +0300

    Updates example

--- example/run_reusable_task.dart
+++ example/run_reusable_task.dart
@@ -1,20 +1,20 @@
 import 'package:ossa/ossa.dart';
 
 void main() async {
-  //
+  // Create and run reusable task using run method
   var task = await Task.run<void>((context) {
     print('Hello, from task!');
   }, type: TaskType.reusable);
 
-  //
+  // Wait when task is completed
   await task.result();
 
-  //
+  // Start task
   await task.start();
 
-  //
+  // Wait when task is completed
   await task.result();
 
-  //
+  // Dispose reusable task
   await task.dispose();
 }
