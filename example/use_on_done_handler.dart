5fb0c752d5cd26a3114ea5de3c622ee345055430
Gleb Batykov <gleb.batykov@gmail.com>
2022-04-19 22:22:49 +0300

    Updates examples

--- example/use_on_done_handler.dart
+++ example/use_on_done_handler.dart
@@ -2,7 +2,7 @@ import 'package:ossa/ossa.dart';
 
 void main() async {
   // Create and run task with int return type, set onDone handler
-  var task = await Task.run<int>((context) {
+  await Task.run<int>((context) {
     return 5 * 10;
   }, onDone: (value) async {
     print(value);
--- example/use_on_error_handler.dart
+++ example/use_on_error_handler.dart
@@ -4,7 +4,7 @@ void main() async {
   late Task task;
 
   // Create and run Task using run method, set onError handler
-  task = await Task.run<void>((context) {
+  task = await Task.run((context) {
     throw FormatException();
   }, onError: (error) async {
     print(error.object.toString());
